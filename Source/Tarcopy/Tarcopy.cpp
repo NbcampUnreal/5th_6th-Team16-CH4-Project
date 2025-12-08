445fa99a81bb4f61049a1955688301d013082044
hanhyur <sunwoohan.me@gmail.com>
2025-12-08 17:33:42 +0900

    feat : 프로젝트 생성

---
+++ Source/Tarcopy/Tarcopy.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tarcopy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tarcopy, "Tarcopy" );
+
+DEFINE_LOG_CATEGORY(LogTarcopy)
+ 
---
+++ Source/Tarcopy/TarcopyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TarcopyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATarcopyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATarcopyGameMode();
+};
+
+
+
