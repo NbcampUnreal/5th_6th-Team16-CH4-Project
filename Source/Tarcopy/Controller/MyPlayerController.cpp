e69cf96eba43467b0aca89a8e946e0151297b6d0
Soggeum <joyten0@gmail.com>
2025-12-10 20:54:41 +0900

    Update MyPlayerController.cpp

--- Source/Tarcopy/Controller/MyPlayerController.cpp
+++ Source/Tarcopy/Controller/MyPlayerController.cpp
@@ -2,4 +2,37 @@
 
 
 #include "Controller/MyPlayerController.h"
+#include <EnhancedInputSubsystems.h>
 
+AMyPlayerController::AMyPlayerController() :
+	IMC_Character(nullptr),
+	MoveAction(nullptr),
+	LookAction(nullptr)
+{
+}
+
+void AMyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!IsLocalController())
+		return;
+
+	FInputModeGameAndUI GameAndUI;
+	SetInputMode(GameAndUI);
+	bShowMouseCursor = true;
+}
+
+void AMyPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (IsLocalPlayerController())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+		{
+			checkf(IMC_Character != nullptr, TEXT("IMC_Character is nullptr"));
+			Subsystem->AddMappingContext(IMC_Character, 0);
+		}
+	}
+}
