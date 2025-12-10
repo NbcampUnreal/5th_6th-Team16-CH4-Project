8996e8fb042a81243118bbad13104d6574d9b238
Soggeum <joyten0@gmail.com>
2025-12-10 20:55:23 +0900

    Update MyCharacter.cpp

--- Source/Tarcopy/Character/MyCharacter.cpp
+++ Source/Tarcopy/Character/MyCharacter.cpp
@@ -2,27 +2,68 @@
 
 
 #include "Character/MyCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
 
 // Sets default values
 AMyCharacter::AMyCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 640.f, 0.f);
+	GetCharacterMovement()->bUseControllerDesiredRotation = false;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->SetUsingAbsoluteRotation(true);
+	SpringArm->TargetArmLength = 2500.f;
+	SpringArm->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	SpringArm->bDoCollisionTest = false;
+
+	// Create the camera component
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
 }
 
 // Called when the game starts or when spawned
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
-// Called every frame
-void AMyCharacter::Tick(float DeltaTime)
+void AMyCharacter::MoveAction(const FInputActionValue& Value)
 {
-	Super::Tick(DeltaTime);
+	if (!IsValid(Controller))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Controller is Invalid."));
+		return;
+	}
 
+	const FVector2D InMovementVector = Value.Get<FVector2D>();
+
+	const FRotator ControlRotation = Controller->GetControlRotation();
+	const FRotator ControlYawRotation(0.0f, ControlRotation.Yaw, 0.f);
+
+	const FVector FowardDirection = FRotationMatrix(ControlYawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(ControlYawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(FowardDirection, InMovementVector.X);
+	AddMovementInput(RightDirection, InMovementVector.Y);
+
+	FVector LookAt = Camera->GetForwardVector();
 }
 
 // Called to bind functionality to input
