3d97b8280eae8560828d4c2898346709eb87ba70
hanhyur <sunwoohan.me@gmail.com>
2025-12-11 21:52:31 +0900

    feat : 멀티플레이 기능
    <description>
    - IP 입력 후 Server에서 가동 중인 Level에 모이도록 구현

--- Source/Tarcopy/UI/UW_TitleScreen.cpp
+++ Source/Tarcopy/UI/UW_TitleScreen.cpp
@@ -6,6 +6,8 @@
 #include "Components/Button.h"
 #include "Components/VerticalBox.h"
 #include "Components/EditableText.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Title/TitlePlayerController.h"
 
 UUW_TitleScreen::UUW_TitleScreen(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -31,16 +33,20 @@ void UUW_TitleScreen::OnStartBtnClicked()
 
 void UUW_TitleScreen::OnOptionBtnClicked()
 {
+	UE_LOG(LogTemp, Log, TEXT("Option button clicked."));
 	OnOptionButtonClicked.Broadcast();
 }
 
 void UUW_TitleScreen::OnExitBtnClicked()
 {
+	UE_LOG(LogTemp, Log, TEXT("Exit button clicked. Quitting game."));
+	UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, false);
 	OnExitButtonClicked.Broadcast();
 }
 
 void UUW_TitleScreen::OnJoinBtnClicked()
 {
 	FText IpPort = IpPortETxt->GetText();
+	UE_LOG(LogTemp, Log, TEXT("Join button clicked with IP/Port: %s"), *IpPort.ToString());	
 	OnJoinButtonClicked.Broadcast(IpPort);
 }
