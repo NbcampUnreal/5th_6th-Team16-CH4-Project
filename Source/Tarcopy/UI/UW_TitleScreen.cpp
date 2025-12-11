920249f55bcba893edcd0da004e187587942ff6d
Derden <khkc9597@gmail.com>
2025-12-11 16:20:37 +0900

    feat : Title UI
    StartBtn
    OptionBtn
    ExitBtn
    IpPort Input
    JoinBtn

---
+++ Source/Tarcopy/UI/UW_TitleScreen.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/UW_TitleScreen.h"
+
+#include "Components/Button.h"
+#include "Components/VerticalBox.h"
+#include "Components/EditableText.h"
+
+UUW_TitleScreen::UUW_TitleScreen(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UUW_TitleScreen::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	StartBtn.Get()->OnClicked.AddDynamic(this, &ThisClass::OnStartBtnClicked);
+	OptionBtn.Get()->OnClicked.AddDynamic(this, &ThisClass::OnOptionBtnClicked);
+	ExitBtn.Get()->OnClicked.AddDynamic(this, &ThisClass::OnExitBtnClicked);
+
+	JoinBtn.Get()->OnClicked.AddDynamic(this, &ThisClass::OnJoinBtnClicked);
+}
+
+void UUW_TitleScreen::OnStartBtnClicked()
+{
+	IpPortInputBox->SetVisibility(ESlateVisibility::Visible);
+	OnStartButtonClicked.Broadcast();
+}
+
+void UUW_TitleScreen::OnOptionBtnClicked()
+{
+	OnOptionButtonClicked.Broadcast();
+}
+
+void UUW_TitleScreen::OnExitBtnClicked()
+{
+	OnExitButtonClicked.Broadcast();
+}
+
+void UUW_TitleScreen::OnJoinBtnClicked()
+{
+	FText IpPort = IpPortETxt->GetText();
+	OnJoinButtonClicked.Broadcast(IpPort);
+}
