73fd00ecf8e713f4d6777c4a22a5ec5529983531
Derden <khkc9597@gmail.com>
2025-12-14 01:51:49 +0900

    add : Inventory관련 UI

---
+++ Source/Tarcopy/Inventory/InventoryData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "InventoryData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TARCOPY_API UInventoryData : public UObject
+{
+	GENERATED_BODY()
+	
+};
