8e3ddc7ad188f85969addf1d9c410a2f9a06213f
Soggeum <joyten0@gmail.com>
2025-12-24 23:30:15 +0900

    Update

--- Source/Tarcopy/Character/Anim/AnimationPreset.h
+++ Source/Tarcopy/Character/Anim/AnimationPreset.h
@@ -1,17 +1,30 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
 #include "AnimationPreset.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class TARCOPY_API UAnimationPreset : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UAnimInstance> IdleAnimation;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UAnimInstance> WalkAnimation;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UAnimInstance> SprintAnimation;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UAnimInstance> CrouchAnimation;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UAnimInstance> HitAnimation;
 	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UAnimInstance> AimAnimation;
 };
--- Source/Tarcopy/Character/Anim/PlayerAnimInstance.h
+++ Source/Tarcopy/Character/Anim/PlayerAnimInstance.h
@@ -1,17 +1,26 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
 #include "PlayerAnimInstance.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class TARCOPY_API UPlayerAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+    virtual void NativeInitializeAnimation() override;
+    virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+    // 애니메이션 변수들 (블루프린트에서 사용)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    float Speed;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    float Direction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+    bool bIsInAir;
+
 };
