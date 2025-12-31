b9ae903345ba0db0c16dae506c423d3e22c99586
Soggeum <joyten0@gmail.com>
2025-12-31 22:42:41 +0900

    Fix Vision
    Vision Mesh collision 수정

--- Source/Tarcopy/Car/TCCarBase.cpp
+++ Source/Tarcopy/Car/TCCarBase.cpp
@@ -241,7 +241,7 @@ void ATCCarBase::PossessedBy(AController* NewController)
 					float ClampedRate = FMath::Clamp(CurrentRPM / 6000.f, 0.f, 1.f);
 					Consumption += ClampedRate * WeakThis->MoveFactor;
 
-					WeakThis->ServerRPCUpdateFuel(-Consumption * 10);
+					WeakThis->ServerRPCUpdateFuel(-Consumption);
 				}),
 			1.f,
 			true,
